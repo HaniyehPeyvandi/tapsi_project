2dc0d764b7cb62106ad98624f6167838413fc73b
HaniyehPeyvandi <hpeyvandi0@gmail.com>
2022-08-29 16:22:11 +0430

    add js code for services section

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
-const toggler = document.querySelector(".nav__toggler");
-const navbar = document.querySelector(".nav");
-toggler.addEventListener("click",(e) => {
-  navbar.classList.toggle("nav__expanded");
-})
+// Toggler
+// const toggler = document.querySelector(".nav__toggler");
+// const navbar = document.querySelector(".nav");
+// toggler.addEventListener("click",(e) => {
+//   navbar.classList.toggle("nav__expanded");
+// });
+
+// Services
+const tabs = document.querySelectorAll(".tab");
+const tabContents = document.querySelectorAll(".tab-content");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click" , (e) => {
+    const targetTabContent = document.querySelector(`#${tab.dataset.tabTarget}`);
+
+    tabs.forEach((tab) => tab.classList.remove("active"));
+    tabContents.forEach((tabContent) => tabContent.classList.remove("active"));
+
+    tab.classList.add("active");
+    targetTabContent.classList.add("active");
+  })
+});
+
